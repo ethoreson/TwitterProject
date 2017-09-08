ee3e54c1b44b3ddc19219567b4d714377093b2b2
Emilie Thoreson <thoresonemilie@gmail.com>
2017-09-08 15:42:25 -0700

    butterknife implementation

--- app/src/main/java/com/epicodus/twitterproject/MainActivity.java
+++ app/src/main/java/com/epicodus/twitterproject/MainActivity.java
@@ -9,25 +9,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.Bind;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRepsButton;
-    private EditText mZipCodeEditText;
-    private TextView mAppIntroTextView;
+    //private static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.findRepsButton) Button mFindRepsButton;
+    @Bind(R.id.zipCodeText) EditText mZipCodeText;
+    @Bind(R.id.appIntroTextView) TextView mAppIntroTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //default behavior, don't change
-        setContentView(R.layout.layout); // use main activity layout
-        mAppIntroTextView = (TextView) findViewById(R.id.appIntroTextView);
+        setContentView(R.layout.activity_main); // use main activity activity_main
+        ButterKinfe.bind(this);
+//
+//        mAppIntroTextView = (TextView) findViewById(R.id.appIntroTextView);
+//        mZipCodeText = (EditText) findViewById(R.id.zipCodeText);
+//        mFindRepsButton = (Button) findViewById(R.id.findRepsButton);
+
         Typeface droidFont = Typeface.createFromAsset(getAssets(), "fonts/DroidSerifRegular.ttf");
         mAppIntroTextView.setTypeface(droidFont);
-        mZipCodeEditText = (EditText) findViewById(R.id.zipCodeText);
-        mFindRepsButton = (Button) findViewById(R.id.findRepsButton);
+
         mFindRepsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String zipCode = mZipCodeEditText.getText().toString();
+                String zipCode = mZipCodeText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, ListOfRepsActivity.class); //new instance of intent class
                 intent.putExtra("zipCode", zipCode);
                 startActivity(intent); //start activity method, takes line above
