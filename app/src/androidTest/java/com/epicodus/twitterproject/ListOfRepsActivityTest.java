c691fb680d8d236a1571676f4681db1a9a1979c9
Emilie Thoreson <thoresonemilie@gmail.com>
2017-09-08 14:54:28 -0700

    populated string array with hard coded representatives, wrote and passed two tests

---
+++ app/src/androidTest/java/com/epicodus/twitterproject/ListOfRepsActivityTest.java
@@ -0,0 +1,40 @@
+package com.epicodus.twitterproject;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+/**
+ * Created by emiliethoreson on 9/8/17.
+ */
+
+public class ListOfRepsActivityTest {
+
+    @Rule
+    public ActivityTestRule<ListOfRepsActivity> activityTestRule =
+            new ActivityTestRule<>(ListOfRepsActivity.class);
+
+    @Test
+    public void listItemClickDisplayToastWithCorrectRepresentative() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String repName = "Jim Ferrell";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(repName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(repName)));
+    }
+}
--- app/src/main/java/com/epicodus/twitterproject/ListOfRepsActivity.java
+++ app/src/main/java/com/epicodus/twitterproject/ListOfRepsActivity.java
@@ -3,17 +3,36 @@ package com.epicodus.twitterproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListOfRepsActivity extends AppCompatActivity {
-    private String[] representatives = new String[] {"Jim Ferrell", "Cyrus Habib", "Jay Inslee", "Dave Reichert", "Patty Murray", "Maria Cantwell", "Mike Pence", "Donald Trump"};
     private TextView mZipCodeTextView;
+    private ListView mListView;
+    private String[] representatives = new String[] {"Jim Ferrell", "Cyrus Habib", "Jay Inslee", "Dave Reichert", "Patty Murray", "Maria Cantwell", "Mike Pence", "Donald Trump"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_reps);
 
+        mListView = (ListView) findViewById(R.id.listView);
         mZipCodeTextView = (TextView) findViewById(R.id.zipCodeTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, representatives);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String representative = ((TextView)view).getText().toString();
+                Toast.makeText(ListOfRepsActivity.this, representative, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String zipCode = intent.getStringExtra("zipCode");
