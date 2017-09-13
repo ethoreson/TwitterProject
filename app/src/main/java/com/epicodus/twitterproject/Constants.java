e60cd4af9c5ec41f8ff3c2507d19635e171ad0c5
Emilie Thoreson <thoresonemilie@gmail.com>
2017-09-13 10:22:36 -0700

    begin Constants.java activities

---
+++ app/src/main/java/com/epicodus/twitterproject/Constants.java
@@ -0,0 +1,5 @@
+package com.epicodus.twitterproject;
+
+public class Constants {
+    public static final String GOOGLE_BASE_URL = "https://www.googleapis.com/civicinfo/v2/representatives?key=<AIzaSyALQDoCiusD0Poqe2mDgGo78zoQy31U2N0>&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS"
+}
---
+++ app/src/main/java/com/epicodus/twitterproject/GoogleService.java
@@ -0,0 +1,8 @@
+package com.epicodus.twitterproject;
+
+public class GoogleService {
+
+    public static void findRepresentatives(String address, Callback callback) {
+
+    }
+}
