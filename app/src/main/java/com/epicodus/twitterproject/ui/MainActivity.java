61299d2bf73d90d6ff7be9f44e1acf442a1225e3
Emilie Thoreson <thoresonemilie@gmail.com>
2017-09-26 12:49:09 -0700

    resolve createaccountyactivity

--- app/src/main/java/com/epicodus/twitterproject/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/twitterproject/ui/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onClick(View v) {
             if (v == mFindRepsButton) {
-                Intent intent = new Intent(MainActivity.this, RepresentativeListActivity.class);
+                Intent intent = new Intent(MainActivity.this, ListOfRepsActivity.class);
                 startActivity(intent);
             }
             if (v == mAboutButton) {
