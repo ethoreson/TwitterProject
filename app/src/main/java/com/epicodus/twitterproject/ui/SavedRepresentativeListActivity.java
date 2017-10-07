1bf1e56bec8e8b2513c5040c77150b9ad913e617
Emilie Thoreson <thoresonemilie@gmail.com>
2017-10-07 14:27:19 -0700

    add push id to model, correct line 26 in savedreps to firebaserepresentativelistadapter

--- app/src/main/java/com/epicodus/twitterproject/ui/SavedRepresentativeListActivity.java
+++ app/src/main/java/com/epicodus/twitterproject/ui/SavedRepresentativeListActivity.java
@@ -4,22 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.epicodus.twitterproject.Constants;
 import com.epicodus.twitterproject.R;
+import com.epicodus.twitterproject.adapters.FirebaseRepresentativeListAdapter;
 import com.epicodus.twitterproject.adapters.FirebaseRepresentativeViewHolder;
 import com.epicodus.twitterproject.models.Representative;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.epicodus.twitterproject.util.OnStartDragListener;
+import com.epicodus.twitterproject.util.SimpleItemTouchHelperCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SavedRepresentativeListActivity extends AppCompatActivity {
+public class SavedRepresentativeListActivity extends AppCompatActivity implements OnStartDragListener {
     private DatabaseReference mRepresentativeReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
-    //private ItemTouchHelper mItemTouchHelper;
+    private FirebaseRepresentativeListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
@@ -29,31 +34,36 @@ public class SavedRepresentativeListActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_list_of_reps);
         ButterKnife.bind(this);
-
-        //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        //String uid = user.getUid();
-
-        mRepresentativeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_REPRESENTATIVES);
         setUpFirebaseAdapter();
     }
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Representative, FirebaseRepresentativeViewHolder>
-                (Representative.class, R.layout.representative_list_item_drag, FirebaseRepresentativeViewHolder.class, mRepresentativeReference) {
 
-            @Override
-            protected void populateViewHolder(FirebaseRepresentativeViewHolder viewHolder,
-                                              Representative model, int position) {
-                viewHolder.bindRepresentative(model);
-            }
-        };
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mRepresentativeReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_REPRESENTATIVES)
+                .child(uid);
+
+        mFirebaseAdapter = new FirebaseRepresentativeListAdapter(Representative.class, R.layout.representative_list_item_drag, FirebaseRepresentativeViewHolder.class, mRepresentativeReference, this, this);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
